bdf9c86f9856babf8bd654634f4c790821119b67
alibaba0010 <zakariyahali100@gmail.com>
2024-12-30 01:10:55 +0100

    Ading page routes and sidebar components

--- apps/restuarant-dashboard/src/app/(routes)/register/page.tsx
+++ apps/restuarant-dashboard/src/app/(routes)/register/page.tsx
@@ -3,7 +3,7 @@ import Register from "../../../shared/views/auth/register";
 const Page = () => {
   return (
     <div className="w-full h-min py-10 flex flex-col justify-center">
-      <h1 className="title">Join to Becodemy</h1>
+      <h1 className="title">SignUp to Alibaba Delivery Services</h1>
       <Register />
     </div>
   );
--- apps/restuarant-dashboard/src/shared/components/layout/Sidenav.tsx
+++ apps/restuarant-dashboard/src/shared/components/layout/Sidenav.tsx
@@ -1,8 +1,25 @@
-import React from "react";
-import { sideBarItems } from "../../../app/configs/constants";
+"use client";
+import { activeItem, sideBarItems } from "../../../app/configs/constants";
+import Link from "next/link";
+import useRouteChange from "../../../hooks/useRouteChange";
 
 const Sidenav = () => {
-  return <>{sideBarItems.map((i: SideBarItemsTypes, index: number) => {})}</>;
+  const { activeRoute, setActiveRoute } = useRouteChange();
+  return (
+    <>
+      {sideBarItems.map((i: SideBarItemsTypes, index: number) => (
+        <Link
+          href={i.url}
+          key={i.url}
+          onClick={() => setActiveRoute(i.url)}
+          className={`${i.url === activeRoute && "text-[rgb(_91_111_230)]"} flex items-center text-2xl px-2`}
+        >
+          <span className="mr-4 text-3xl">{i.icon}</span>
+          {i.title}
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default Sidenav;
